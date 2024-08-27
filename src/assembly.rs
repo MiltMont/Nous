cc6bcdcc03365c1672fc87441e718f69b1e8376c
MiltMont <miltgonmont@pm.me>
2024-08-26 21:42:37 -0600

    refactor: Update Assembly AST to use AssemblyInstruction instead of Instruction

--- src/assembly.rs
+++ src/assembly.rs
@@ -1,4 +1,3 @@
-
 use crate::ast::Identifier;
 
 #[derive(Debug, Clone)]
@@ -7,33 +6,33 @@ pub struct AssemblyProgram(pub AssemblyFunction);
 #[derive(Debug, Clone)]
 pub struct AssemblyFunction {
     pub name: Identifier,
-    pub instructions: Vec<Instruction>,
+    pub instructions: Vec<AssemblyInstruction>,
 }
 
 #[derive(Debug, Clone)]
-pub enum Instruction {
+pub enum AssemblyInstruction {
     Mov { src: Operand, dst: Operand },
-    Unary(UnaryOperator, Operand), 
-    AllocateStack(i64), 
+    Unary(UnaryOperator, Operand),
+    AllocateStack(i64),
     Ret,
 }
 
 #[derive(Debug, Clone)]
 pub enum UnaryOperator {
-    Neg, 
-    Not
+    Neg,
+    Not,
 }
 
 #[derive(Debug, Clone)]
 pub enum Operand {
     Imm(i64),
     Register(Reg),
-    Pseudo(Identifier), 
-    Stack(i64)
+    Pseudo(Identifier),
+    Stack(i64),
 }
 
 #[derive(Debug, Clone)]
 pub enum Reg {
-    AX, 
-    R10, 
+    AX,
+    R10,
 }
