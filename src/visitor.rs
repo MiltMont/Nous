d659e3d664e1fd7959b78402d9b1c9210a441b51
Milton Montiel <59279510+MiltMont@users.noreply.github.com>
2024-10-22 19:22:30 +0000

    fix: Fix typing issues.

--- src/visitor.rs
+++ src/visitor.rs
@@ -1,5 +1,7 @@
+use crate assembly::{Program, Instruction};
+
 struct AssemblyPass {
-    assembly: Assembly, 
+    program: Program,
     instructions: Vec<Instruction>
 }
 
@@ -9,15 +11,7 @@ impl AssemblyPass {
     /// field is not None, otherwise it 
     /// returns an Error.  
     pub fn build(assembly: assembly::Program) -> Result<Self, String>{
-        if let Some(program) = assembly.program {
-            Self {
-                assembly: assembly, 
-                instructions: program.0.instructions
-            }
-        } else {
-            // TODO: Give a more precise message.
-            Err("No program found. Try parsing the program first.".to_string())
-        }
+        todo!()
     }
 
     // TODO: Implement 
@@ -28,26 +22,26 @@ impl AssemblyPass {
     // allocate_stack()
     // 
 
-    pub fn replace_pseudo_registers(&mut self) -> mut Self {
+    pub fn replace_pseudo_registers(&mut self) -> &mut Self {
         todo!()
     }
 
-    pub fn rewrite_mov(&mut self) -> mut Self {
+    pub fn rewrite_mov(&mut self) -> &mut Self {
         todo!()
     } 
 
-    pub fn rewrite_binop(&mut self) -> mut Self {
+    pub fn rewrite_binop(&mut self) -> &mut Self {
         todo!()
     }
 
-    pub fn allocate_stack(&mut self) -> mut Self {
+    pub fn allocate_stack(&mut self) -> &mut Self {
         todo!()
     }
 
     /// Replaces the instruction set on 
     /// the original assembly value and returns
     /// the modified instance. 
-    pub fn modify_assembly(self) -> Assembly {
+    pub fn modify_assembly(self) -> Program {
         todo!()
     }
 }
