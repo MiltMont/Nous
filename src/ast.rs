850397749e7e19bb2f4786168de0f580a78caacd
MiltMont <miltgonmont@pm.me>
2024-10-24 12:36:40 -0600

    feat: Improve debug formatting for ast

--- src/ast.rs
+++ src/ast.rs
@@ -31,9 +31,29 @@ pub enum Expression {
     Binary(BinaryOperator, Box<Expression>, Box<Expression>),
 }
 
-#[derive(Debug, PartialEq, Clone)]
+// impl Debug for Expression {
+//     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+//         match self {
+//             Self::Constant(arg0) => f.debug_tuple("Constant").field(arg0).finish(),
+//             Self::Unary(arg0, arg1) => f.debug_tuple("Unary").field(arg0).field(arg1).finish(),
+//             Self::Binary(arg0, arg1, arg2) => {
+//                 write!(f, "Binary({:?}, \n\t{:?}, {:?})", arg0, arg1, arg2)
+//             }
+//         }
+//     }
+// }
+
+#[derive(PartialEq, Clone)]
 pub enum Statement {
     Return(Expression),
+}
+
+impl Debug for Statement {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::Return(arg0) => write!(f, "Return(\n{:#?} \n\t\t)", arg0),
+        }
+    }
 }
 
 #[derive(PartialEq, Clone, Hash, Eq)]
@@ -45,11 +65,27 @@ impl Debug for Identifier {
     }
 }
 
-#[derive(Debug, PartialEq, Clone)]
+#[derive(PartialEq, Clone)]
 pub struct Function {
     pub name: Identifier,
     pub body: Statement,
 }
 
-#[derive(Debug, PartialEq, Clone)]
+impl Debug for Function {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "Function(\n\t\tname: {:?} \n\t\tbody: \n\t\t{:?}\n\t)",
+            &self.name.0, &self.body
+        )
+    }
+}
+
+#[derive(PartialEq, Clone)]
 pub struct Program(pub Function);
+
+impl Debug for Program {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "Program(\n\t{:?}\n)", &self.0)
+    }
+}
